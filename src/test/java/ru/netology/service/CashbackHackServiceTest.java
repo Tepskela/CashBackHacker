44dc785a285fd3503d25d37b07626b54f960d7bd
Aleksandr Petrenko <aleksandr_petrenko62@mail.ru>
2023-05-16 21:49:46 +0300

    2testNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,10 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+    }
+}
